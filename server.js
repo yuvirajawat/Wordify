92ffb32ec013d885e608c4d40dea660a8dcb82e4
YUVRAJ SINGH RAJAWAT <yuvirajawat193@gmail.com>
2022-05-16 11:35:48 +0530

    css fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 // to get values from html form
 app.use(bodyParser.urlencoded({extended: true}));
 //static css files on server
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(__dirname+"/public"));
 
 app.use(session({
   secret:process.env.SECRET,
